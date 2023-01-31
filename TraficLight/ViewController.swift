7a9d8ce5459dfedf4af176f9ae14cd79257bae79
Georgii Litvinenko <jyrki.ss@gmail.com>
2023-01-31 17:10:57 +0200

    if else

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
+        redView.layer.cornerRadius = 50
+        yellowView.layer.cornerRadius = 50
+        greenView.layer.cornerRadius = 50
+        
     }
-
-
+    
+    @IBAction func startButtonTapped() {
+        
+        startButton.setTitle("Next", for: .normal)
+        
+        if redView.alpha != 1 && yellowView.alpha != 1 && greenView.alpha != 1 {
+            redView.alpha = 1
+        } else if redView.alpha == 1 && yellowView.alpha != 1 && greenView.alpha != 1 {
+            yellowView.alpha = 1
+            redView.alpha = 0.3
+        } else if redView.alpha != 1 && yellowView.alpha == 1 && greenView.alpha != 1 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else {
+            redView.alpha = 0.3
+            redView.alpha = 0.3
+            greenView.alpha = 0.3
+        }
+        
+    }
 }
 
